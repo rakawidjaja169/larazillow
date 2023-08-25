2ed6d7ac79a3fcc56cfa415e4ada63bb37d83c1b
rakawidjaja169 <raka.widjaja169@gmail.com>
2023-08-25 20:24:24 +0700

    Fix Bugs
    - Positioned and Sized the Table

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+  <div class="relative overflow-x-auto shadow-md sm:rounded-lg flex justify-center">
+    <table class="max-w-7xl text-sm text-left text-gray-500 dark:text-gray-400">
       <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
         <tr>
           <th scope="col" class="p-4">
