6444f4a232ae73e6f7f12bcd778e5fe991242e47
rakawidjaja169 <raka.widjaja169@gmail.com>
2023-08-25 21:09:27 +0700

    Fix Bugs
    - Make Listing Table instead Listing Row only

---
+++ resources/js/Components/ListingTable.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="relative overflow-x-auto shadow-md sm:rounded-lg flex justify-center">
+    <table class="max-w-7xl text-sm text-left text-gray-500 dark:text-gray-400">
+      <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <tr>
+          <th scope="col" class="p-4">
+            <div class="flex items-center">
+              <input id="checkbox-all-search" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+              <label for="checkbox-all-search" class="sr-only">checkbox</label>
+            </div>
+          </th>
+          <th scope="col" class="px-6 py-3">
+            Product name
+          </th>
+          <th scope="col" class="px-6 py-3">
+            Quantity
+          </th>
+          <th scope="col" class="px-6 py-3">
+            Address
+          </th>
+          <th scope="col" class="px-6 py-3">
+            Price
+          </th>
+          <th scope="col" class="px-6 py-3">
+            Action
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="listing in listings"
+          :key="listing.id" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+        >
+          <td class="w-4 p-4">
+            <div class="flex items-center">
+              <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+              <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+            </div>
+          </td>
+          <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            <Link :href="route('listing.show', { listing: listing.id })">
+              {{ listing.products }}
+            </Link>
+          </th>
+          <td class="px-6 py-4">
+            {{ listing.quantity }}
+          </td>
+          <td class="px-6 py-4">
+            {{ listing.address }}
+          </td>
+          <td class="px-6 py-4">
+            <Price :price="listing.price" />
+          </td>
+          <td class="flex items-center px-6 py-4 space-x-3">
+            <Link :href="route('listing.edit', { listing: listing.id })" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</Link>
+            <Link :href="route('listing.destroy', { listing: listing.id })" method="DELETE" as="button" class="font-medium text-red-600 dark:text-red-500 hover:underline">Remove</Link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3'
+import Price from '@/Components/Price.vue'
+defineProps({
+  listings: Object,
+})
+</script>
--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,56 +1,9 @@
 <template>
-  <div class="relative overflow-x-auto shadow-md sm:rounded-lg flex justify-center">
-    <table class="max-w-7xl text-sm text-left text-gray-500 dark:text-gray-400">
-      <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-        <tr>
-          <th scope="col" class="p-4">
-            <div class="flex items-center">
-              <input id="checkbox-all-search" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-              <label for="checkbox-all-search" class="sr-only">checkbox</label>
-            </div>
-          </th>
-          <th scope="col" class="px-6 py-3">
-            Product name
-          </th>
-          <th scope="col" class="px-6 py-3">
-            Quantity
-          </th>
-          <th scope="col" class="px-6 py-3">
-            Address
-          </th>
-          <th scope="col" class="px-6 py-3">
-            Price
-          </th>
-          <th scope="col" class="px-6 py-3">
-            Action
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="listing in listings"
-          :key="listing.id" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
-        >
-          <td class="w-4 p-4">
-            <div class="flex items-center">
-              <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-              <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
-            </div>
-          </td>
-          <ListingProduct :listing="listing" />
-          <td class="flex items-center px-6 py-4 space-x-3">
-            <Link :href="route('listing.edit', { listing: listing.id })" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</Link>
-            <Link :href="route('listing.destroy', { listing: listing.id })" method="DELETE" as="button" class="font-medium text-red-600 dark:text-red-500 hover:underline">Remove</Link>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+  <ListingTable :listings="listings" />
 </template>
 
 <script setup>
-import { Link } from '@inertiajs/inertia-vue3'
-import ListingProduct from '@/Components/ListingProduct.vue'
+import ListingTable from '@/Components/ListingTable.vue'
 defineProps({
   listings: Array,
 })
