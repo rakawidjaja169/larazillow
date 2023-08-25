04b12322c0624e390ad84937f0b462627306fce9
rakawidjaja169 <raka.widjaja169@gmail.com>
2023-08-25 23:29:28 +0700

    Styling Forms

--- resources/js/Pages/Listing/Create.vue
+++ resources/js/Pages/Listing/Create.vue
@@ -1,6 +1,6 @@
 <template>
   <form @submit.prevent="create">
-    <div class="grid grid-cols-6 gap-4 max-w-7xl justify-center">
+    <div class="grid grid-cols-6 gap-4">
       <div class="col-span-4">
         <label class="block mb-1 text-gray-500 dark:text-gray-300 font-medium">Products</label>
         <input v-model="form.products" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500" />
@@ -17,32 +17,32 @@
         </div>
       </div>
   
-      <div>
-        <label>Description</label>
-        <input v-model="form.description" type="text" />
+      <div class="col-span-6">
+        <label class="block mb-1 text-gray-500 dark:text-gray-300 font-medium">Description</label>
+        <input v-model="form.description" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500" />
         <div v-if="form.errors.description">
           {{ form.errors.description }}
         </div>
       </div>
   
-      <div>
-        <label>Address</label>
-        <input v-model="form.address" type="text" />
+      <div class="col-span-6">
+        <label class="block mb-1 text-gray-500 dark:text-gray-300 font-medium">Address</label>
+        <input v-model="form.address" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500" />
         <div v-if="form.errors.address">
           {{ form.errors.address }}
         </div>
       </div>
   
-      <div>
-        <label>Price</label>
-        <input v-model.number="form.price" type="text" />
+      <div class="col-span-6">
+        <label class="block mb-1 text-gray-500 dark:text-gray-300 font-medium">Price</label>
+        <input v-model.number="form.price" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500" />
         <div v-if="form.errors.price">
           {{ form.errors.price }}
         </div>
       </div>
   
-      <div>
-        <button type="submit">Create</button>
+      <div class="col-span-6">
+        <button type="submit" class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rounded-md">Create</button>
       </div>
     </div>
   </form>
