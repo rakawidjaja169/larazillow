a9a2bcf7e70d12bcfcae2be3a3e3dfd21d4c8c35
rakawidjaja169 <raka.widjaja169@gmail.com>
2023-08-26 23:59:12 +0700

    User Registration Logic & Hashing Password
    - User Registration Logic
    - Hashing Passwords

--- routes/web.php
+++ routes/web.php
@@ -33,4 +33,4 @@ Route::delete('logout', [AuthController::class, 'destroy'])
     ->name('logout');
 
 Route::resource('user-account', UserAccountController::class)
-    ->only(['create']);
+    ->only(['create', 'store']);
