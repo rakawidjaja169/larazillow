9cdcf05a84b4d516ef37225167290fcd5eec6a0a
rakawidjaja169 <raka.widjaja169@gmail.com>
2023-08-26 22:47:47 +0700

    Getting the Currently Logged In User
    - Share the Data
    - Fix Main Layout for Logged In User or Not

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -50,12 +50,15 @@
 
           <!-- Profile dropdown -->
           <Menu as="div" class="relative ml-3">
-            <div>
+            <div v-if="user">
               <MenuButton class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                 <span class="absolute -inset-1.5" />
                 <span class="sr-only">Open user menu</span>
                 <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
               </MenuButton>
+            </div>
+            <div v-else>
+              <Link :href="route('login')">Sign-In</Link>
             </div>
             <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
               <MenuItems class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
@@ -111,6 +114,10 @@ const navigation = [
 const page = usePage()
 const flashSuccess = computed(
   () => page.props.value.flash.success,
+)
+
+const user = computed(
+  () => page.props.value.user,
 )
 
 // console.log('Current route:', route().current())
