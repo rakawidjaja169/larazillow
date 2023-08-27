5f5286f07d425047de6131d16f237e540cb52fc5
rakawidjaja169 <raka.widjaja169@gmail.com>
2023-08-27 19:17:32 +0700

    Showing the User Listings
    - Showing the User Listings

--- app/Http/Controllers/UserListingController.php
+++ app/Http/Controllers/UserListingController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserListingController extends Controller
 {
     public function index()
     {
-        return inertia('User/Index');
+        return inertia(
+            'User/Index',
+            ['listings' => Auth::user()->listings]
+        );
     }
 }
--- resources/js/Pages/User/Index.vue
+++ resources/js/Pages/User/Index.vue
@@ -1,3 +1,34 @@
 <template>
-  Say hello!
+  <h1 class="text-3xl mb-4">Your Listings</h1>
+  <section class="mb-8">Filters</section>
+  <section class="grid grid-cols-1 lg:grid-cols-2 gap-2">
+    <Box v-for="listing in listings" :key="listing.id">
+      <div class="flex flex-col md:flex-row gap-2 md:items-center justify-between">
+        <div>
+          <div class="xl:flex items-center gap-2">
+            <Price :price="listing.price" class="text-2xl font-medium" />
+          </div>
+          <ListingProduct :listing="listing" class="text-lg" />
+          <ListingDescription :listing="listing" />
+          <ListingAddress :listing="listing" class="text-gray-500" />
+        </div>
+        <div class="flex items-center gap-1 text-gray-600 dark:text-gray-300">
+          <Link class="btn-outline text-xs font-medium">Preview</Link>
+          <Link class="btn-outline text-xs font-medium">Edit</Link>
+          <Link class="btn-outline text-xs font-medium">Delete</Link>
+        </div>
+      </div>
+    </Box>
+  </section>
 </template>
+
+<script setup>
+import ListingProduct from '@/Components/ListingProduct.vue'
+import ListingDescription from '@/Components/ListingDescription.vue'
+import ListingAddress from '@/Components/ListingAddress.vue'
+import Price from '@/Components/Price.vue'
+import Box from '@/Components/UI/Box.vue'
+import { Link } from '@inertiajs/inertia-vue3'
+
+defineProps({listings: Array})
+</script>
