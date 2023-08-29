fc0d0fb82ed0f3a36ce33ecc2e2cc2b9eabbe662
rakawidjaja169 <raka.widjaja169@gmail.com>
2023-08-29 22:06:23 +0700

    Fix Bugs

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -1,76 +1,77 @@
 <template>
-  <div class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4 w-9/12">
-    <Box class="md:col-span-7 flex items-center">
-      <div v-if="listing.images.length" class="grid grid-cols-2 gap-1">
-        <img
-          v-for="image in listing.images" :key="image.id"
-          :src="image.src"
-        />
-      </div>
-      <div v-else class="w-full text-center font-medium text-gray-500">No images</div>
-    </Box>
-    <div class="md:col-span-5 flex flex-col gap-4">
-      <Box>
-        <template #header>
-          Basic info
-        </template>
-        <Price :price="listing.price" class="text-2xl font-bold" />
-        <div class="text-lg">
-          <ListingProduct :listing="listing" />
-        </div>
-        <div>
-          <ListingDescription :listing="listing" />
-        </div>
-        <div class="text-gray-500">
-          <ListingAddress :listing="listing" />
-        </div>
-      </Box>
-
-      <Box>
-        <template #header>
-          Monthly Payment
-        </template>
-        <div>
-          <label class="label">Interest rate ({{ interestRate }}%)</label>
-          <input
-            v-model.number="interestRate"
-            type="range" min="0.1" max="30" step="0.1"
-            class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+  <div class="w-full flex justify-center">
+    <div class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4 w-9/12">
+      <Box class="md:col-span-7 flex items-center">
+        <div v-if="listing.images.length" class="grid grid-cols-2 gap-1">
+          <img
+            v-for="image in listing.images" :key="image.id"
+            :src="image.src"
           />
-
-          <label class="label">Duration ({{ duration }} years)</label>
-          <input
-            v-model.number="duration"
-            type="range" min="3" max="35" step="1"
-            class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
-          />
-
-          <div class="text-gray-600 dark:text-gray-300 mt-2">
-            <div class="text-gray-400">Your monthly payment</div>
-            <Price :price="monthlyPayment" class="text-3xl" />
-          </div>
         </div>
-        <div class="mt-2 text-gray-500">
-          <div class="flex justify-between">
-            <div>Total paid</div>
-            <div>
-              <Price :price="totalPaid" class="font-medium" />
-            </div>
-          </div>
-          <div class="flex justify-between">
-            <div>Principal paid</div>
-            <div>
-              <Price :price="listing.price" class="font-medium" />
-            </div>
-          </div>
-          <div class="flex justify-between">
-            <div>Interest paid</div>
-            <div>
-              <Price :price="totalInterest" class="font-medium" />
-            </div>
-          </div>
-        </div>
+        <div v-else class="w-full text-center font-medium text-gray-500">No images</div>
       </Box>
+      <div class="md:col-span-5 flex flex-col gap-4">
+        <Box>
+          <template #header>
+            Basic info
+          </template>
+          <Price :price="listing.price" class="text-2xl font-bold" />
+          <div class="text-lg">
+            <ListingProduct :listing="listing" />
+          </div>
+          <div>
+            <ListingDescription :listing="listing" />
+          </div>
+          <div class="text-gray-500">
+            <ListingAddress :listing="listing" />
+          </div>
+        </Box>
+
+        <Box>
+          <template #header>
+            Monthly Payment
+          </template>
+          <div>
+            <label class="label">Interest rate ({{ interestRate }}%)</label>
+            <input
+              v-model.number="interestRate"
+              type="range" min="0.1" max="30" step="0.1"
+              class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+            />
+
+            <label class="label">Duration ({{ duration }} years)</label>
+            <input
+              v-model.number="duration"
+              type="range" min="3" max="35" step="1"
+              class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+            />
+
+            <div class="text-gray-600 dark:text-gray-300 mt-2">
+              <div class="text-gray-400">Your monthly payment</div>
+              <Price :price="monthlyPayment" class="text-3xl" />
+            </div>
+          </div>
+          <div class="mt-2 text-gray-500">
+            <div class="flex justify-between">
+              <div>Total paid</div>
+              <div>
+                <Price :price="totalPaid" class="font-medium" />
+              </div>
+            </div>
+            <div class="flex justify-between">
+              <div>Principal paid</div>
+              <div>
+                <Price :price="listing.price" class="font-medium" />
+              </div>
+            </div>
+            <div class="flex justify-between">
+              <div>Interest paid</div>
+              <div>
+                <Price :price="totalInterest" class="font-medium" />
+              </div>
+            </div>
+          </div>
+        </Box>
 
       <MakeOffer :listing-id="listing.id" />
     </div>
