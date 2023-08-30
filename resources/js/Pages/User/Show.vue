26d4e3a05d045810f68b47eecaa54f017e4e3c82
rakawidjaja169 <raka.widjaja169@gmail.com>
2023-08-30 22:53:54 +0700

    Seeing Which Offer is Accepted
    - Seeing Which Offer is Accepted

--- resources/js/Pages/User/Show.vue
+++ resources/js/Pages/User/Show.vue
@@ -15,29 +15,34 @@
         </div>
       </Box>
 
-      <div v-else class="md:col-span-7 items-center">
+      <div v-else class="md:col-span-7 flex flex-col gap-4">
         <Offer
           v-for="offer in listing.offers" 
-          :key="offer.id" class="mb-4"
+          :key="offer.id"
           :offer="offer"
         />
       </div>
 
-      <Box class="md:col-span-5">
-        <template #header>
-          Basic info
-        </template>
-        <Price :price="listing.price" class="text-2xl font-bold" />
-        <div class="text-lg">
-          <ListingProduct :listing="listing" :quantity="listing.quantity" />
-        </div>
-        <div>
-          <ListingDescription :listing="listing" />
-        </div>
-        <div class="text-gray-500">
-          <ListingAddress :listing="listing" />
-        </div>
-      </Box>
+      <div class="md:col-span-5">
+        <Box class="md:col-span-5">
+          <template #header>
+            Basic info
+          </template>
+          <Price 
+            :price="listing.price" 
+            class="text-2xl font-bold" 
+          />
+          <div class="text-lg">
+            <ListingProduct :listing="listing" :quantity="listing.quantity" />
+          </div>
+          <div>
+            <ListingDescription :listing="listing" />
+          </div>
+          <div class="text-gray-500">
+            <ListingAddress :listing="listing" />
+          </div>
+        </Box>
+      </div>
     </section>
   </div>
 </template>
