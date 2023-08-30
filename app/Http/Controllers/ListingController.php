e58da431a64eb01cb6944c7b28d898b1ac34b17c
rakawidjaja169 <raka.widjaja169@gmail.com>
2023-08-30 22:57:14 +0700

    Filtering Out Accepted Offers (Querying Relationship Presence/Absence)
    - Filtering Out Accepted Offers (Querying Relationship Presence/Absence)

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -35,6 +35,7 @@ class ListingController extends Controller
                 'filters' => $filters,
                 'listings' => Listing::mostRecent()
                     ->filter($filters)
+                    ->withoutSold()
                     ->paginate(10)
                     ->withQueryString()
             ]
